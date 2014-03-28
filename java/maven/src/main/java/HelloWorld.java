f541dc939805761f85de5dae98a6b8e519948005
jtong <tj19832@gmail.com>
2014-03-28 15:23:24 +0800

    jtong - add mock example

---
+++ java/maven/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by twer on 3/28/14.
+ */
+public class HelloWorld {
+    public String beenCalled() {
+        return "Leave me alone.";
+    }
+}
--- java/maven/src/test/java/HelloWorldTest.java
+++ java/maven/src/test/java/HelloWorldTest.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class HelloWorldTest {
-   @Test
-   public void hello_world_test(){
+    @Test
+    public void hello_world_test(){
        assertThat("Hello World").isEqualTo("Hello World");
-   }
+    }
+
+    @Test
+    public void should_be_mocked(){
+        HelloWorld helloWorld = mock(HelloWorld.class);
+        when(helloWorld.beenCalled()).thenReturn("Hello World");
+
+        assertThat(helloWorld.beenCalled()).isEqualTo("Hello World");
+    }
 }
