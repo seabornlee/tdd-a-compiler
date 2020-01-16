9e91e49ccc81eb07c720ba289182503bf3c80ec0
Seaborn Lee <seabornlee.cn@gmail.com>
2020-01-17 07:08:13 +0800

    chore: migrate to JUnit 5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
--- src/test/java/scanner/TokenTest.java
+++ src/test/java/scanner/TokenTest.java
@@ -1,6 +1,6 @@
 package scanner;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
