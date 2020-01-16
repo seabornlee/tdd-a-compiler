c00efcd6c76d64336ea2e9ff90f60b8a60a2e011
Seaborn Lee <seabornlee.cn@gmail.com>
2020-01-17 07:24:25 +0800

    test: refactor test

--- src/test/java/ScannerTest.java
+++ src/test/java/ScannerTest.java
@@ -8,15 +8,23 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ScannerTest {
     @Test
-    public void should_generate_token() {
-        String source = "count = 1";
+    public void should_generate_tokens_from_source_code() {
+        String source = "position = initial + rate * 60";
         List<Token> tokens = new Scanner().scan(source);
-        assertThat(tokens).hasSize(3);
+        assertThat(tokens).hasSize(7);
         assertThat(tokens.get(0).getName()).isEqualTo("id");
-        assertThat(tokens.get(0).getValue()).isEqualTo("count");
+        assertThat(tokens.get(0).getValue()).isEqualTo("position");
         assertThat(tokens.get(1).getName()).isEqualTo("=");
         assertThat(tokens.get(1).getValue()).isNull();
-        assertThat(tokens.get(2).getName()).isEqualTo("1");
-        assertThat(tokens.get(2).getValue()).isNull();
+        assertThat(tokens.get(2).getName()).isEqualTo("id");
+        assertThat(tokens.get(2).getValue()).isEqualTo("initial");
+        assertThat(tokens.get(3).getName()).isEqualTo("+");
+        assertThat(tokens.get(3).getValue()).isNull();
+        assertThat(tokens.get(4).getName()).isEqualTo("id");
+        assertThat(tokens.get(4).getValue()).isEqualTo("rate");
+        assertThat(tokens.get(5).getName()).isEqualTo("*");
+        assertThat(tokens.get(5).getValue()).isNull();
+        assertThat(tokens.get(6).getName()).isEqualTo("60");
+        assertThat(tokens.get(6).getValue()).isNull();
     }
 }
